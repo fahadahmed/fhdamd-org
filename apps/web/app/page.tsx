909a60c7e52ccddb1a69c9ffacbf54624de7534e
Fahad Ahmed <fahad.ahmed@me.com>
2025-10-11 17:16:55 +1100

    #5 - renamed ui packages to threads

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,5 +1,5 @@
 import Image, { type ImageProps } from "next/image";
-import { Button } from "../../../packages/ui/src/button";
+import { Button } from "@fhdamd/threads/button";
 import styles from "./page.module.css";
 
 type Props = Omit<ImageProps, "src"> & {
