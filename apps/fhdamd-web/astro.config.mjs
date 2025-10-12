26dde2e2273d4ba44f8c108646b62af0a40255ae
Fahad Ahmed <fahad.ahmed@me.com>
2025-10-12 18:44:04 +1100

    #3 - added react to the astro project

--- apps/fhdamd-web/astro.config.mjs
+++ apps/fhdamd-web/astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import node from '@apphosting/astro-adapter';
+import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
@@ -8,4 +9,6 @@ export default defineConfig({
   adapter: node({
     mode: 'standalone',
   }),
+
+  integrations: [react()],
 });
