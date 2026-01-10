b57bf0ee81bcd1d7e2dd9d8ee6606fde67d51a3a
Fahad Ahmed <fahad.ahmed@me.com>
2026-01-10 16:08:15 +1100

    fix(pdf-craft): minor style fixes and query updates
    minor style fixes and query updates
    
    #61

--- apps/pdf-craft/functions/src/cms/queries.ts
+++ apps/pdf-craft/functions/src/cms/queries.ts
@@ -1,9 +1,11 @@
 export const CMS_QUERIES = {
   faqs: `
-    query AllFaqs {
-      allFaqs {
+    query MyQuery {
+      allFaqs(orderBy: _createdAt_ASC) {
         title
         content
+        id
+        _createdAt
       }
     }
   `,
