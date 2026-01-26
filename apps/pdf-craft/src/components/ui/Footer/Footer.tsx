b598ec8c464dbe36ef262521196021b3a2a56b6f
Fahad Ahmed <fahad.ahmed@me.com>
2026-01-27 00:22:23 +1100

    style(pdf-craft): updated the footer styles and added to login and register routes
    updated the footer styles and added to login and register routes
    
    #65

--- apps/pdf-craft/src/components/ui/Footer/Footer.tsx
+++ apps/pdf-craft/src/components/ui/Footer/Footer.tsx
@@ -6,8 +6,13 @@ export default function Footer() {
 
   return (
     <footer className="footer-container">
-      <div>pdf-craft</div>
-      <small>Copyright &copy; {today} - PDF Craft</small>
+      <h6 className="footer-logo">pdf <small>craft</small></h6>
+      <ul className="footer-links">
+        <li><a href="/terms">Terms & Conditions</a></li>
+        <li><a href="/privacy">Privacy Policy</a></li>
+        <li><a href="/contact">Contact Us</a></li>
+        <li><a href="/resources">Resources</a></li>
+      </ul>
     </footer>
   )
 }
