fc10a2d51eca7c41220c23ea696849d4482eedc9
Fahad Ahmed <fahad.ahmed@me.com>
2026-01-29 15:12:21 +1100

    refactor(pdf-craft): header component rewrite
    header component rewrite
    
    #64

--- apps/pdf-craft/src/components/ui/Header/Header.tsx
+++ apps/pdf-craft/src/components/ui/Header/Header.tsx
@@ -1,9 +1,12 @@
 'use client'
+import { useState } from 'react'
 import { actions } from 'astro:actions'
 import { useRecaptcha } from '../../../utils'
+import { Button } from '../../'
 import './header.css'
 
 export default function Header() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const captchaToken = useRecaptcha('logout');
 
   const handleLogout = async (e: React.FormEvent) => {
@@ -26,10 +29,17 @@ export default function Header() {
 
   return (
     <div className="header">
-      <div><strong>PDF-Craft</strong></div>
-      <form onSubmit={handleLogout}>
-        <button type="submit">Logout</button>
-      </form>
+      <div className="header-logo"><a href="/">pdf <small>craft</small></a></div>
+      {isLoggedIn ? (
+        <form onSubmit={handleLogout}>
+          <button type="submit">Logout</button>
+        </form>
+      ) : (
+        <div className="header-links">
+          <Button kind="tertiary" type="linkButton" url="/signin" text="Login" />
+          <Button kind="secondary" type="button" text="Sign Up" onClick={() => window.location.href = '/signup'} />
+        </div>
+      )}
     </div>
   )
 }
