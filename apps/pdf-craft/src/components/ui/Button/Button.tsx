dd55c9a782d76c366de27c2eed0f2acbcab6ad07
Fahad Ahmed <fahad.ahmed@me.com>
2026-01-29 15:07:59 +1100

    refactor(pdf-craft): button component rewrite
    button component rewrite
    
    #64

--- apps/pdf-craft/src/components/ui/Button/Button.tsx
+++ apps/pdf-craft/src/components/ui/Button/Button.tsx
@@ -1,16 +1,23 @@
 import './button.css'
 
 export type ButtonProps = {
-  type: 'button' | 'linkButton' | 'submit',
-  text: string,
-  url?: string,
-  kind?: "primary" | "secondary" | "tertiary",
+  type: 'button' | 'linkButton' | 'submit'
+  text: string
+  url?: string
+  kind?: 'primary' | 'secondary' | 'tertiary'
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-export default function Button({ type, text, url, kind }: ButtonProps) {
-  return (
-    type === 'linkButton' ?
-      <a href={url} className="link-button">{text}</a>
-      : <button type={type} className={kind}>{text}</button>
+export default function Button({ type, text, url, kind = 'primary', onClick }: ButtonProps) {
+  const className = `btn ${kind}`
+
+  return type === 'linkButton' ? (
+    <a href={url} className={className}>
+      {text}
+    </a>
+  ) : (
+    <button type={type} className={className} onClick={onClick}>
+      {text}
+    </button>
   )
 }
